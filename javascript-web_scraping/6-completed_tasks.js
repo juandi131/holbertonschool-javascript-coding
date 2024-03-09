6f8a0c0611897a51a29b29f5e03ec997c5f3bbce
juandi131 <124542502+juandi131@users.noreply.github.com>
2024-03-09 20:01:33 -0300

    12

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,44 +1,32 @@
-const express = require('express');
-const { readFile } = require('fs').promises;
+#!/usr/bin/node
+const request = require('request');
 
-async function countStudents(path) {
-  try {
-    const data = await readFile(path, 'utf-8');
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
+const apiUrl = process.argv[2];
 
-    const studentData = lines.slice(1).map((line) => line.split(','));
+request(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const todo = JSON.parse(body);
 
-    const cs = [];
-    const swe = [];
-    for (let i = 0; i < studentData.length; i += 1) {
-      if (studentData[i][3] === 'CS') {
-        cs.push(studentData[i][0]);
-      } else if (studentData[i][3] === 'SWE') {
-        swe.push(studentData[i][0]);
+    const completedTasksByUser = {};
+
+    // Parcourt les tâches dans la réponse JSON.
+    for (const task of todo) {
+      // Vérifie si la tâche est complétée.
+      if (task.completed) {
+        // Obtient l'ID de l'utilisateur de la tâche.
+        const userId = task.userId;
+
+        if (completedTasksByUser[userId]) {
+          completedTasksByUser[userId]++;
+        } else {
+        // user a accompli sa 1er tâche complétée.
+          completedTasksByUser[userId] = 1;
+        }
       }
     }
-    return (`Number of students: ${studentData.length}\nNumber of students in CS: ${cs.length}. List: ${cs.join(', ')}\nNumber of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
-  } catch (error) {
-    return 'Cannot load the database';
+
+    console.log(completedTasksByUser);
+  } else {
+    console.error(error || `Code d'état ${response.statusCode}`);
   }
-}
-
-const app = express();
-const port = 1245;
-const path = process.argv[2];
-
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
 });
-
-app.get('/students', async (req, res) => {
-  const title = 'This is the list of our students\n';
-  const result = await countStudents(path);
-  res.send(title + result);
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
-module.exports = app;
