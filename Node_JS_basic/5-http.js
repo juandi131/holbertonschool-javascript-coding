133602d96ae186d6763f5330f8bd06f7682daff3
juandi131 <124542502+juandi131@users.noreply.github.com>
2024-03-09 17:23:00 -0300

    12

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,37 +1,35 @@
 const http = require('http');
-const fs = require('fs').promises;
 
-const countStudents = async (filePath) => {
-  try {
-    const data = await fs.readFile(filePath, 'utf8');
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
 
-    return `Number of students: ...\nNumber of students in CS: ...`;
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-};
+const DATABASE = args[0];
+
+const hostname = '127.0.0.1';
+const port = 1245;
 
 const app = http.createServer(async (req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const { url } = req;
+
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
     try {
-      const filePath = process.argv[2]; // La ruta del archivo CSV se pasa como argumento
-      const text = await countStudents(filePath);
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end(`This is the list of our students\n${text}`);
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
     } catch (error) {
-      res.writeHead(500);
       res.end(error.message);
     }
-  } else {
-    res.writeHead(404);
-    res.end('Not found');
   }
+  res.statusCode = 404;
+  res.end();
 });
 
-app.listen(1245);
-console.log('Server listening on port 1245');
+app.listen(port, hostname, () => {
+});
 
 module.exports = app;
