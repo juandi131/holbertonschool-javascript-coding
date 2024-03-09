6a43c6721accf49ae97fc96b5a762df36e1cdb22
juandi131 <124542502+juandi131@users.noreply.github.com>
2024-03-09 18:25:14 -0300

    12

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,25 +1,45 @@
 const express = require('express');
-const students = require('./3-read_file_async');
+const fs = require('fs');
+const countStudents = require('./3-read_file_async');
 
 const app = express();
-const fs = require('fs');
+const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  res.write('This is the list of our students\n');
-  await students(process.argv[2]).then((data) => {
-    res.write(`Number of students: ${data.students.length}\n`);
-    res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
-    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
-  }).catch((err) => res.write(err.message))
-    .finally(() => {
-      res.end();
-    });
+app.get('/students', (req, res) => {
+  if (fs.existsSync(process.argv[2])) {
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.write('This is the list of our students\n');
+        const lines = data;
+        const getField = {};
+        lines.forEach((line) => {
+          getField[line[line.length - 1]] = getField[line[line.length - 1]] + 1 || 1;
+        });
+        res.write(`Number of students: ${lines.length}\n`);
+
+        for (const field in getField) {
+          if (field) {
+            const names = lines.filter((line) => line[line.length - 1] === field)
+              .map((name) => name[0]);
+            res.write(`Number of students in ${field}: ${getField[field]}. List: ${names.join(', ')}\n`);
+          }
+        }
+        res.end();
+      })
+      .catch((error) => {
+        res.end(error.message);
+      });
+  } else {
+    res.send('This is the list of our students\nCannot load the database');
+  }
 });
 
-app.listen(1245);
+app.listen(port, () => {
+  console.log(`My app is listening on port ${port}`);
+});
 
 module.exports = app;
