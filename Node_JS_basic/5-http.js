a3bab3e7a14bf70fae37dd5c20cd17a706af1e2e
juandi131 <124542502+juandi131@users.noreply.github.com>
2024-03-09 17:17:30 -0300

    12

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,35 +1,35 @@
 const http = require('http');
-
-const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
-const DATABASE = args[0];
-
-const hostname = '127.0.0.1';
+const DATABASE = process.argv[2];
 const port = 1245;
 
 const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-
   const { url } = req;
 
   if (url === '/') {
-    res.write('Hello Holberton School!');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
   } else if (url === '/students') {
-    res.write('This is the list of our students\n');
     try {
       const students = await countStudents(DATABASE);
-      res.end(`${students.join('\n')}`);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(`This is the list of our students\n${students}`);
     } catch (error) {
-      res.end(error.message);
+      res.statusCode = 500;
+      res.end(`Error: ${error.message}`);
     }
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not found');
   }
-  res.statusCode = 404;
-  res.end();
 });
 
-app.listen(port, hostname, () => {
+app.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
