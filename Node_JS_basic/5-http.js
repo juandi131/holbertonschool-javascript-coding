f3f7dc90429cc3eec990c3abded8c8986359c56c
juandi131 <124542502+juandi131@users.noreply.github.com>
2024-03-09 17:11:16 -0300

    12

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,21 +1,28 @@
 const http = require('http');
-const students = require('./3-read_file_async');
+const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-  const asked = (req.url);
-  if (asked === '/') {
-    res.writeHead(200);
-    res.end('Hello Holberton School!');
-  }
-  if (asked === '/students') {
-    res.writeHead(200);
-    res.write('This is the list of our students\n');
-    students(process.argv[2])
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Hello Holberton School!');
+    res.end();
+  } else if (req.url === '/students') {
+    res.statusCode = 200;
+    let consoleOutput = '';
+
+    const originalConsoleLog = console.log;
+    console.log = (message) => {
+      consoleOutput += `${message}\n`;
+    };
+    countStudents(process.argv[2])
       .then((data) => {
-        res.end(data);
+        res.end(`This is the list of our students\n${consoleOutput.trim()}`);
       })
       .catch((error) => {
-        res.end(error.message);
+        res.statusCode = 400;
+        res.write(`This is the list of our students\nCannot load the database`);
+        res.end();
       });
   }
 }).listen(1245);
