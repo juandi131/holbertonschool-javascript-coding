64870649773114cd8e32e05df227a48dc9b1b092
juandi131 <124542502+juandi131@users.noreply.github.com>
2024-03-09 17:54:33 -0300

    12

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,25 +1,20 @@
 #!/usr/bin/node
-
-const r = require('request');
-r(process.argv[2], (e, res) => {
-  const completed = [];
-  const usrs = {};
-  let x = 0;
-
-  for (const user of JSON.parse(res.body)) {
-    if (user.completed === true) {
-      completed.push(user.userId);
-    }
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
   }
 
-  for (const id of completed) {
-    for (const user of JSON.parse(res.body)) {
-      if (user.completed === true) {
-        if (user.userId === id) { x += 1; }
+  const users = {};
+  for (const task of JSON.parse(body)) {
+    if (task.completed) {
+      if (users[task.userId]) {
+        users[task.userId]++;
+      } else {
+        users[task.userId] = 1;
       }
     }
-    usrs[id] = x;
-    x = 0;
   }
-  console.log(usrs);
+  console.log(users);
 });
