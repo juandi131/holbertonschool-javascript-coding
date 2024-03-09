c203f11d912f7742956e040c4d4dc3523b09ab94
juandi131 <124542502+juandi131@users.noreply.github.com>
2024-03-09 19:57:26 -0300

    12

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,29 +1,28 @@
-#!/usr/bin/node
+const express = require('express');
+const countStudents = require('./3-read_file_async');
 
+const app = express();
+const filePath = process.argv[2];
 
-const request = require('request');
-
-
-request(process.argv[2], function (error, response, body) {
-
-  if (!error) {
-
-    const todos = JSON.parse(body);
-
-
-    const completed = {};
-
-
-    todos.forEach((todo) => {
-
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1; 
-      } else if (todo.completed) {
-        completed[todo.userId] += 1; 
-      }
-    });
-
-
-    console.log(completed);
-  }
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
 });
+
+app.get('/students', (req, res) => {
+  res.write('This is the list of our students\n');
+  countStudents(filePath)
+    .then((data) => {
+      res.write(data.join('\n'));
+      res.end();
+    })
+    .catch(() => {
+      res.end('Cannot load the database');
+    });
+});
+
+app.listen(1245, () => {
+  console.log('Listening on port 1245');
+});
+
+module.exports = app;
+
