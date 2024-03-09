6e40ae21fd9748ac46b7ecf44204679cc032d483
juandi131 <124542502+juandi131@users.noreply.github.com>
2024-03-09 19:58:39 -0300

    12

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,28 +1,44 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const { readFile } = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    const data = await readFile(path, 'utf-8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    const studentData = lines.slice(1).map((line) => line.split(','));
+
+    const cs = [];
+    const swe = [];
+    for (let i = 0; i < studentData.length; i += 1) {
+      if (studentData[i][3] === 'CS') {
+        cs.push(studentData[i][0]);
+      } else if (studentData[i][3] === 'SWE') {
+        swe.push(studentData[i][0]);
+      }
+    }
+    return (`Number of students: ${studentData.length}\nNumber of students in CS: ${cs.length}. List: ${cs.join(', ')}\nNumber of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+  } catch (error) {
+    return 'Cannot load the database';
+  }
+}
 
 const app = express();
-const filePath = process.argv[2];
+const port = 1245;
+const path = process.argv[2];
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  res.write('This is the list of our students\n');
-  countStudents(filePath)
-    .then((data) => {
-      res.write(data.join('\n'));
-      res.end();
-    })
-    .catch(() => {
-      res.end('Cannot load the database');
-    });
+app.get('/students', async (req, res) => {
+  const title = 'This is the list of our students\n';
+  const result = await countStudents(path);
+  res.send(title + result);
 });
 
-app.listen(1245, () => {
-  console.log('Listening on port 1245');
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
 module.exports = app;
-
