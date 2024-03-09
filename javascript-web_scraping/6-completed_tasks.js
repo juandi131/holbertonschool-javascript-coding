10e7a6db407dbbf10aedadb84f0d66e5ba586b39
juandi131 <124542502+juandi131@users.noreply.github.com>
2024-03-09 16:29:15 -0300

    12

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -3,17 +3,22 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (err, _response, body) {
+request(url, (err, res, body) => {
   if (err) {
     console.log(err);
-  } else {
-    const users = JSON.parse(body);
-    const completed = users.reduce((acc, user) => {
-      if (user.completed) {
-        acc[user.userId] = (acc[user.userId] || 0) + 1;
-      }
-      return acc;
-    }, {});
-    console.log(completed);
+    return;
   }
+  const arr = [];
+  const tasks = JSON.parse(body).filter(val => val.completed === true);
+  tasks.forEach(function (task) {
+    if (arr[task.userId - 1] == null) {
+      arr[task.userId - 1] = 0;
+    }
+    arr[task.userId - 1] += 1;
+  });
+  const obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    obj[i + 1] = arr[i];
+  }
+  console.log(obj);
 });
