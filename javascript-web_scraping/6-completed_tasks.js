ebb5df4f6ed1d01b29e8bfd2ee7641c89b0dcbc3
juandi131 <124542502+juandi131@users.noreply.github.com>
2024-03-09 22:22:24 -0300

    12

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,30 +1,21 @@
-const express = require('express');
-const countStudents = require('./3-read_file_async');
+#!/usr/bin/node
 
-const app = express();
-/* eslint-disable */
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
+const request = require('request');
+const url = process.argv[2];
 
-app.get('/students', async (req, res) => {
-  try {
-    let consoleOutput = '';
-    const originalConsoleLog = console.log; // Almacena la función original
-
-    console.log = (message) => {
-      consoleOutput += `${message}\n`; // Captura la salida de console.log
-    };
-
-    await countStudents(process.argv[2]);
-
-    console.log = originalConsoleLog; // Restaura la función original de console.log
-
-    res.send(`This is the list of our students\n${consoleOutput.trim()}`);
-  } catch (error) {
-    res.send(`This is the list of our students\n${error.message}`);
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
   }
+  const completed = {};
+  for (const task of JSON.parse(body)) {
+    if (task.completed === true) {
+      if (completed[task.userId]) {
+        completed[task.userId]++;
+      } else {
+        completed[task.userId] = 1;
+      }
+    }
+  }
+  console.log(completed);
 });
-app.listen(1245);
-
-module.exports = app;
